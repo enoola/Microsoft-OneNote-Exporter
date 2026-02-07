54884c153a4859ac49dee71dd16a0c34e5ea05af
enoola <enola@next.local>
2026-02-07 21:21:16 +0100

    Added: bash_aliases.sh, in a terminal in root folder of the project do source bash_aliases.sh to have few useful aliases at your fingertip

---
+++ bash_aliases.sh
@@ -0,0 +1,6 @@
+#aliases .. for ONE OneNote Exporter
+#launch onenote list
+alias one-login='node src/index.js login'
+alias one-list='node src/index.js list'
+alias oned-export='node src/index.js export --dodump --notheadless --nopassasked'
+
